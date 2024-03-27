f46642753b4b4bbaf415d033871dfe4b377b0451
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-03-27 15:04:23 +0530

    make div center using flex

---
+++ object.js
@@ -0,0 +1,21 @@
+// object literals
+let person = {
+    firstName:"viraj",lastName:"beelagi",age:1
+}
+
+//constructor function
+function Person(firstName,age){
+    this.firstName = firstName,
+    this.age = age
+}
+let person1 = new Person("yash",40)
+let person2 = new Person("veer",35)
+
+//class syntax
+class Person {
+    constructor(firstName,lastName,age){
+        this.firstName = firstName,
+        this.lastName = lastName,
+        this.age = age
+    }     
+}
