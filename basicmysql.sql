ce26225bc5a68c9a9acf050992a4b97b004f6f8b
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-04-02 20:44:15 +0530

    MySQL HAVING clause example3

--- basicmysql.sql
+++ basicmysql.sql
@@ -229,3 +229,18 @@ GROUP BY ordernumber
 HAVING 
     total > 1000 AND 
     itemsCount > 600;
+
+    SELECT 
+    a.ordernumber, 
+    status, 
+    SUM(priceeach*quantityOrdered) total
+FROM
+    orderdetails a
+INNER JOIN orders b 
+    ON b.ordernumber = a.ordernumber
+GROUP BY  
+    ordernumber, 
+    status
+HAVING 
+    status = 'Shipped' AND 
+    total > 1500;
