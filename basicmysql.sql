9b171e3f7b584641149822f42677eedd86e92219
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-04-09 17:06:19 +0530

    DATE_SUB function

--- basicmysql.sql
+++ basicmysql.sql
@@ -615,4 +615,12 @@ SELECT
        DATE_ADD(@dt, INTERVAL 1 MONTH) '1 month later',
        DATE_ADD(@dt, INTERVAL 1 YEAR) '1 year later';
 
-       
+    --  MySQL DATE_SUB function
+    SELECT @dt start, 
+       DATE_SUB(@dt, INTERVAL 1 SECOND) '1 second before',
+       DATE_SUB(@dt, INTERVAL 1 MINUTE) '1 minute before',
+       DATE_SUB(@dt, INTERVAL 1 HOUR) '1 hour before',
+       DATE_SUB(@dt, INTERVAL 1 DAY) '1 day before',
+       DATE_SUB(@dt, INTERVAL 1 WEEK) '1 week before',
+       DATE_SUB(@dt, INTERVAL 1 MONTH) '1 month before',
+       DATE_SUB(@dt, INTERVAL 1 YEAR) '1 year before';
