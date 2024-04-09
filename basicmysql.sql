01faeb1073df67dbc93f59da99a538e5822eed46
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-04-09 17:03:46 +0530

    MySQL DATE_ADD function

--- basicmysql.sql
+++ basicmysql.sql
@@ -604,3 +604,15 @@ SELECT
     MONTH(@dt),
     QUARTER(@dt),
     YEAR(@dt);
+
+    -- MySQL DATE_ADD function
+    SELECT @dt start, 
+       DATE_ADD(@dt, INTERVAL 1 SECOND) '1 second later',
+       DATE_ADD(@dt, INTERVAL 1 MINUTE) '1 minute later',
+       DATE_ADD(@dt, INTERVAL 1 HOUR) '1 hour later',
+       DATE_ADD(@dt, INTERVAL 1 DAY) '1 day later',
+       DATE_ADD(@dt, INTERVAL 1 WEEK) '1 week later',
+       DATE_ADD(@dt, INTERVAL 1 MONTH) '1 month later',
+       DATE_ADD(@dt, INTERVAL 1 YEAR) '1 year later';
+
+       
