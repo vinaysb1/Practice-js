01256ce81583d93abbbce227d6162109112cfed3
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-06-30 20:30:58 +0530

    Replace Substring

--- strings.js
+++ strings.js
@@ -16,3 +16,8 @@ let anotherName = "Vishal";
 console.log(firstName.localeCompare(anotherName));
 let anotherName1 = "Vaishali";
 console.log(firstName.localeCompare(anotherName1));
+
+// Replace Substring
+const str = "vinay is best frontend developer.vinay is best developer.";
+console.log(str.replace("vinay","sanjay"));
+console.log(str.replaceAll("vinay","sanjay"));
