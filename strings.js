9a2655fc25e83087c82499b7803dd52e2534d089
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-06-30 20:27:08 +0530

    strings length,presence ,accessing

---
+++ strings.js
@@ -0,0 +1,18 @@
+//length of string
+let firstName = "Vaishali";
+console.log(firstName.length);
+//Access of string
+console.log(firstName.charAt(2));
+console.log(firstName[2]);
+console.log(firstName.charCodeAt(2));
+//Check Presence of Character
+console.log(firstName.includes("r"));
+console.log(firstName.includes("a"));
+console.log(firstName.indexOf("s"));
+console.log(firstName.indexOf("v"));
+console.log(firstName.lastIndexOf("i"));
+// Compare Two Strings
+let anotherName = "Vishal";
+console.log(firstName.localeCompare(anotherName));
+let anotherName1 = "Vaishali";
+console.log(firstName.localeCompare(anotherName1));
