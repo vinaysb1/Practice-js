f386824d6c24aab597393fffab6b5a4c79870851
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-07-24 06:43:00 +0530

    Sort Array by Increasing Frequency  approach -2

--- arrays/sortArraybyIncreasingFreq.js
+++ arrays/sortArraybyIncreasingFreq.js
@@ -8,4 +8,11 @@ var frequencySort = function(nums) {
     return nums.sort((a,b) => counter[a] === counter[b] ? b - a: counter[a] - counter[b])
 };
 
-// 
+// Approach-2
+var frequencySort = function (nums) {
+    let obj = {};
+    for (let num of nums) {
+        obj[num] = (obj[num] || 0) + 1;
+    }
+    return nums.sort((a, b) => obj[a] === obj[b] ? b - a : obj[a] - obj[b]);
+};
