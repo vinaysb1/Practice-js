d1ffc356e315fb7c7aa3b6a529811c0766b33c45
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-08-01 18:40:46 +0530

    conditional ternary operator

--- js_basics/lesson1.js
+++ js_basics/lesson1.js
@@ -24,3 +24,13 @@ console.log(calculator(10, 5, "+")); // Output: 15
 console.log(calculator(10, 5, "-")); // Output: 5
 console.log(calculator(10, 5, "*")); // Output: 50
 console.log(calculator(10, 5, "/")); // Output: 2
+
+// conditional ternary operator 
+// syntax : condition?exp1:exp2
+let age = 18
+let canVote = (age>=18) ? "eligible" : "noteligible"
+console.log(canVote)
+
+let isMember = true;
+let discount = (isMember) ? 1:5;
+console.log(discount);
