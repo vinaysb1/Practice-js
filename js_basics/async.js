798893d09524bd7b18b2012843902819351d96bb
vinaysb1 <vinaykumarbeelagi@gmail.com>
2024-08-03 21:09:44 +0530

    async examples

--- js_basics/async.js
+++ js_basics/async.js
@@ -24,3 +24,36 @@ await delay(2000);
 console.log("End")
 }
 run()
+
+// example-3 basic async awit function
+const getData =async()=>{
+    let y = await "hello india";
+    console.log(y)
+}
+getData()
+
+// ex-4 promise function
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then(response => response.json())
+.then(json => console.log(json))
+.catch(error => console.log(error))
+// above same function can write using async
+
+async function runProcess() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+    const json = await response.json()
+    console.log(json)
+}
+runProcess();
+
+// above same example using try ,catch with arrow
+const runProcess1 = async()=> {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const json = await response.json()
+        console.log(json)
+    }catch (error){
+        console.log(error)
+    }
+};
+runProcess1();
